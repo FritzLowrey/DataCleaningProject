6a4be80d03059df6c17f3c70b634b60ea82710ad
fritz-lowrey <fritz_lowrey@yahoo.com>
2015-03-11 14:07:28 -0400

    update

--- README.md
+++ README.md
@@ -1,17 +1,28 @@
 # Getting and Cleaning Data
 ## Read-me for Course Project
-### Fritz Lowrey (jaflowrey@gmail.com)
-### 11 March, 2015
+### Fritz Lowrey (jaflowrey@gmail.com) 11 March, 2015
 
-*Description*
-blah blah blah
+####Description
+This script will run and compose a filtered list of all mean and std values gathered from the source data sets. It then summarizes the input by subject and activity by generating the mean of each captured parameter.
 
+####Functional Logic
+The core logic in this tool merges the test and training data in the files:
 
+	test\X_test.txt
+	test\Y_test.txt
+	test\subjects_test.txt
+	train\X_train.txt
+	train\Y_train.txt
+	train\subjects_train.txt
+	
+The inputs are merged into a data frame containing the mean and standard deviation values for the collected data, as defined in:
 
-*Functional Logic*
-asddsaf
+    *XXX*
+	
+Once the consolidated, filtered data is completed the data frame is saved to disk from which it can be loaded in the future to avoid re-computation of all results - as inputs will not change. This file is called "TRIMMED.txt", a codebook for the contents of this file is in this GitHub repository.
 
+Once the system has the consolidated, filtered data (either from the runtime logic or by loading from disk) it is summarized by participant and activity. The code creates lists of unique subjects and unique activities from the main data frame, then for each subject+activity combination takes the subset of the data and computes a mean of the data for each variable excluding NA values. The results of each summary are added to an output data frame for saving to disk. The codebook for this file is: XXXXX
 
-*References*
+####References
 asdsfda
 asdfdsa
--- run_analysis.R
+++ run_analysis.R
@@ -109,9 +109,8 @@ getBigFrame <- function(baseDir)
     fBodyBodyGyroMag.std  = numeric(), #= lapply(full.list, "[", 532), #530 fBodyBodyGyroMag-std()
     
     fBodyBodyGyroJerkMag.mean  = numeric(), #= lapply(full.list, "[", 544), #542 fBodyBodyGyroJerkMag-mean()
-    fBodyBodyGyroJerkMag.std  = numeric() #= lapply(full.list, "[", 545) #543 fBodyBodyGyroJerkMag-std()
-	) ## end of dataframe
-	
+    fBodyBodyGyroJerkMag.std  = numeric(), #= lapply(full.list, "[", 545) #543 fBodyBodyGyroJerkMag-std()
+    stringsAsFactors =FALSE) ## end of dataframe	
 	
 	# memory hungry as heck with 500+ values per line and 10k+ record, but it makes the DF creation much easier and we should GC after we return
   full.list <- lapply(strsplit(readLines(paste(testFolder, "X_test.txt", sep = "/"))," "), as.numeric)
@@ -120,7 +119,9 @@ getBigFrame <- function(baseDir)
   thisRow <- 1
   while(thisRow < length(full.list))
   {
+    #debug printout, but it's useful to see that processing is taking place.
     print(thisRow)
+    
     aRow <- full.list[[thisRow]]
     aRow <- aRow[c(3:length(aRow))] # trim the first 2 elements
     
@@ -132,13 +133,132 @@ getBigFrame <- function(baseDir)
                             227,228,240,241,253,254,266,267,268,269,270,271,345,346,347,348,349,350,424,425,426,427,428,429,503,504,516,517,529,
                             530,542,543)])  	                        
   	
-  	#big.frame[nrow(big.frame)+1, ] <- thisList    
-    big.frame <- rbind(big.frame, setNames(thisList, names(big.frame)), default.stringsasfactors=FALSE)
-  	
+  	#appending a list to a DF is surprisingly annoying if you want to keep column names 
+    rowDF <- data.frame(thisList, stringsAsFactors =FALSE)
+    rowDF <- setNames(rowDF, names(big.frame))
+    big.frame <- rbind(big.frame, rowDF)
+    
+    #next row
   	thisRow <- thisRow + 1
   }
   
-  return(big.frame)    
+  # no point in passing this big object around, so push it into a global value
+  global.frame <<- big.frame
+  
+  return()
+}
+
+# SummarizeInfo
+# get the data from global.frame for the given participant and activity. For each data column (3:...) compute a mean disregarding NAs
+# return a list of this subject, activity, and all computed values to be added to the output data.frame
+summarizeInfo <- function(thisSubject, thisActivity)
+{
+  thisSummary <- list(thisSubject, thisActivity)
+  
+  slice <- global.frame[global.frame$subject == thisSubject & global.frame$activity == thisActivity,];  
+  
+  for(thisCol in 3:length(names(slice)))
+  {
+    anAvg <- mean(na.omit(slice[,thisCol]))
+    thisSummary <- append(thisSummary, anAvg)
+  }
+  
+  return(thisSummary)
+  
+}
+
+# create an empty data frame for the summary data
+makeSummaryDF <- function()
+{
+  summaryDF <- data.frame(
+    subject = integer(), 
+    activity = character(),
+    mean.tBodyAcc.mean.X = numeric(), # lapply(full.list, "[", 3),
+    mean.tBodyAcc.mean.Y = numeric(), #= lapply(full.list, "[", 4),
+    mean.tBodyAcc.mean.Z = numeric(), #= lapply(full.list, "[", 5),
+    mean.tBodyAcc.std.X  = numeric(), #= lapply(full.list, "[", 6),
+    mean.tBodyAcc.std.Y  = numeric(), #= lapply(full.list, "[", 7),
+    mean.tBodyAcc.std.Z  = numeric(), #= lapply(full.list, "[", 8), #
+    
+    mean.tGravityAcc.mean.X  = numeric(), #= lapply(full.list, "[", 43), #41 tGravityAcc.mean.X
+    mean.tGravityAcc.mean.Y  = numeric(), #= lapply(full.list, "[", 44), #42 tGravityAcc.mean.Y
+    mean.tGravityAcc.mean.Z  = numeric(), #= lapply(full.list, "[", 45), #43 tGravityAcc.mean.Z
+    mean.tGravityAcc.std.X  = numeric(), #= lapply(full.list, "[", 46), #44 tGravityAcc.std.X
+    mean.tGravityAcc.std.Y  = numeric(), #= lapply(full.list, "[", 47), #45 tGravityAcc.std.Y
+    mean.tGravityAcc.std.Z  = numeric(), #= lapply(full.list, "[", 48), #46 tGravityAcc.std.Z
+    
+    mean.tBodyAccJerk.mean.X  = numeric(), #= lapply(full.list, "[", 83), #81 tBodyAccJerk.mean.X
+    mean.tBodyAccJerk.mean.Y  = numeric(), #= lapply(full.list, "[", 84), #82 tBodyAccJerk.mean.Y
+    mean.tBodyAccJerk.mean.Z  = numeric(), #= lapply(full.list, "[", 85), #83 tBodyAccJerk.mean.Z
+    mean.tBodyAccJerk.std.X  = numeric(), #= lapply(full.list, "[", 86), #84 tBodyAccJerk.std.X
+    mean.tBodyAccJerk.std.Y  = numeric(), #= lapply(full.list, "[", 87), #85 tBodyAccJerk.std.Y
+    mean.tBodyAccJerk.std.Z  = numeric(), # = lapply(full.list, "[", 88), #86 tBodyAccJerk.std.Z
+    
+    mean.tBodyGyro.mean.X  = numeric(), #= lapply(full.list, "[", 123), #121 tBodyGyro.mean.X
+    mean.tBodyGyro.mean.Y  = numeric(), #= lapply(full.list, "[", 124), #122 tBodyGyro.mean.Y
+    mean.tBodyGyro.mean.Z  = numeric(), #= lapply(full.list, "[", 125), #123 tBodyGyro.mean.Z
+    mean.tBodyGyro.std.X  = numeric(), #= lapply(full.list, "[", 126), #124 tBodyGyro.std.X
+    mean.tBodyGyro.std.Y  = numeric(), #= lapply(full.list, "[", 127), #125 tBodyGyro.std.Y
+    mean.tBodyGyro.std.Z  = numeric(), #= lapply(full.list, "[", 128), #126 tBodyGyro.std.Z
+    
+    mean.tBodyGyroJerk.mean.X  = numeric(), #= lapply(full.list, "[", 163), #161 tBodyGyroJerk-mean()-X
+    mean.tBodyGyroJerk.mean.Y  = numeric(), #= lapply(full.list, "[", 164), #162 tBodyGyroJerk-mean()-Y
+    mean.tBodyGyroJerk.mean.Z  = numeric(), #= lapply(full.list, "[", 165), #163 tBodyGyroJerk-mean()-Z
+    mean.tBodyGyroJerk.std.X  = numeric(), #= lapply(full.list, "[", 166), #164 tBodyGyroJerk-std()-X
+    mean.tBodyGyroJerk.std.Y  = numeric(), #= lapply(full.list, "[", 167), #165 tBodyGyroJerk-std()-Y
+    mean.tBodyGyroJerk.std.Z  = numeric(), #= lapply(full.list, "[", 168), #166 tBodyGyroJerk-std()-Z
+    
+    mean.tBodyAccMag.mean  = numeric(), #= lapply(full.list, "[", 203), #201 tBodyAccMag-mean()
+    mean.tBodyAccMag.std   = numeric(), #= lapply(full.list, "[", 204), #202 tBodyAccMag-std()
+    
+    mean.tGravityAccMag.mean  = numeric(), #= lapply(full.list, "[", 216), #214 tGravityAccMag-mean()
+    mean.tGravityAccMag.std   = numeric(), #= lapply(full.list, "[", 217), #215 tGravityAccMag-std()
+    
+    mean.tBodyAccJerkMag.mean  = numeric(), #= lapply(full.list, "[", 229), #227 tBodyAccJerkMag-mean()
+    mean.tBodyAccJerkMag.std   = numeric(), #= lapply(full.list, "[", 230), #228 tBodyAccJerkMag-std()
+    
+    mean.tBodyGyroMag.mean  = numeric(), #= lapply(full.list, "[", 242), #240 tBodyGyroMag-mean()
+    mean.tBodyGyroMag.std   = numeric(), #= lapply(full.list, "[", 243), #241 tBodyGyroMag-std()
+    
+    mean.tBodyGyroJerkMag.mean  = numeric(), #= lapply(full.list, "[", 255), #253 tBodyGyroJerkMag-mean()
+    mean.tBodyGyroJerkMag.std   = numeric(), #= lapply(full.list, "[", 256), #254 tBodyGyroJerkMag-std()
+    
+    mean.fBodyAcc.mean.X  = numeric(), #= lapply(full.list, "[", 268), #266 fBodyAcc-mean()-X
+    mean.fBodyAcc.mean.Y  = numeric(), #= lapply(full.list, "[", 269), #267 fBodyAcc-mean()-Y
+    mean.fBodyAcc.mean.Z  = numeric(), #= lapply(full.list, "[", 270), #268 fBodyAcc-mean()-Z
+    mean.fBodyAcc.std.X  = numeric(), #= lapply(full.list, "[", 271), #269 fBodyAcc-std()-X
+    mean.fBodyAcc.std.Y  = numeric(), #= lapply(full.list, "[", 272), #270 fBodyAcc-std()-Y
+    mean.fBodyAcc.std.Z  = numeric(), #= lapply(full.list, "[", 273), #271 fBodyAcc-std()-Z
+    
+    mean.fBodyAccJerk.mean.X  = numeric(), #= lapply(full.list, "[", 347), #345 fBodyAccJerk-mean()-X
+    mean.fBodyAccJerk.mean.Y  = numeric(), #= lapply(full.list, "[", 348), #346 fBodyAccJerk-mean()-Y
+    mean.fBodyAccJerk.mean.Z  = numeric(), #= lapply(full.list, "[", 349), #347 fBodyAccJerk-mean()-Z
+    mean.fBodyAccJerk.std.X  = numeric(), #= lapply(full.list, "[", 350), #348 fBodyAccJerk-std()-X
+    mean.fBodyAccJerk.std.Y  = numeric(), #= lapply(full.list, "[", 352), #349 fBodyAccJerk-std()-Y
+    mean.fBodyAccJerk.std.Z  = numeric(), #= lapply(full.list, "[", 353), #350 fBodyAccJerk-std()-Z
+    
+    mean.fBodyGyro.mean.X  = numeric(), #= lapply(full.list, "[", 426), #424 fBodyGyro-mean()-X
+    mean.fBodyGyro.mean.Y  = numeric(), #= lapply(full.list, "[", 427), #425 fBodyGyro-mean()-Y
+    mean.fBodyGyro.mean.Z  = numeric(), #= lapply(full.list, "[", 428), #426 fBodyGyro-mean()-Z
+    mean.fBodyGyro.std.X  = numeric(), #= lapply(full.list, "[", 429), #427 fBodyGyro-std()-X
+    mean.fBodyGyro.std.Y  = numeric(), #= lapply(full.list, "[", 430), #428 fBodyGyro-std()-Y
+    mean.fBodyGyro.std.Z  = numeric(), #= lapply(full.list, "[", 431), #429 fBodyGyro-std()-Z
+    
+    mean.fBodyAccMag.mean  = numeric(), #= lapply(full.list, "[", 505), #503 fBodyAccMag-mean()
+    mean.fBodyAccMag.std  = numeric(), #= lapply(full.list, "[", 506), #504 fBodyAccMag-std()
+    
+    mean.fBodyBodyAccJerkMag.mean  = numeric(), #= lapply(full.list, "[", 518), #516 fBodyBodyAccJerkMag-mean()
+    mean.fBodyBodyAccJerkMag.std  = numeric(), #= lapply(full.list, "[", 519), #517 fBodyBodyAccJerkMag-std()
+    
+    mean.fBodyBodyGyroMag.mean  = numeric(), #= lapply(full.list, "[", 531), #529 fBodyBodyGyroMag-mean()
+    mean.fBodyBodyGyroMag.std  = numeric(), #= lapply(full.list, "[", 532), #530 fBodyBodyGyroMag-std()
+    
+    mean.fBodyBodyGyroJerkMag.mean  = numeric(), #= lapply(full.list, "[", 544), #542 fBodyBodyGyroJerkMag-mean()
+    mean.fBodyBodyGyroJerkMag.std = numeric(),
+    stringsAsFactors = FALSE    
+    )
+  
+  return(summaryDF)
 }
 
 # run_analysis: Perform consolidation and summarization of Sample input
@@ -160,26 +280,49 @@ run_analysis <- function(baseDir = getwd())
 
   tableFile <- paste(baseDir, "TRIMMED.txt", sep="/")
   
+  # for debugging, if the file has already been created, load and use it. Otherwise, compute it from the input data
   if(!file.exists(tableFile))
   {
-    big.frame <- getBigFrame(baseDir)
+    # sets up global.frame based on input data
+    getBigFrame(baseDir)
     
-    write.table(big.frame, file = tableFile, row.names=FALSE)    
+    # save the consolidated file for later use
+    write.table(global.frame, file = tableFile, row.names=FALSE)    
   }
   else
   {
-    big.frame <- read.table(header=TRUE, file=tableFile)
+    # load the consolidated file to save time during development
+    global.frame <<- read.table(header=TRUE, file=tableFile)
   }	
   
-  return("Complete")
+  # now that I have a populated combined DF, time to summarize by subject and activity
+  participants <- unique(global.frame$subject)
+  activities <- unique(global.frame$activity)
   
-
-  #trainFolder <- paste(getwd(), "train", sep="/")
-  #trainSubjects <- file.read(paste(trainFolder, "subject_train.txt", sep = "/"))  
-  #trainX <- file.read(paste(trainFolder, "X_train.txt", sep = "/"))  
-  #trainY <- file.read(paste(trainFolder, "Y_train.txt", sep = "/"))  
+  # create my empty data.frame with all headers
+  dfSummary <- makeSummaryDF()
   
-  # Merge subjects list
-  #foreach subject
+  for(thisPart in participants)
+  {
+    for(thisActivity in activities)
+    {
+      listRow <- summarizeInfo(thisPart, thisActivity)
+      dfRow <- data.frame(listRow, stringsAsFactors=FALSE)
+      dfRow <- setNames(dfRow, names(dfSummary))
+      
+      dfSummary <- rbind(dfSummary, dfRow)      
+    }
+  }
   
+  # compose my output file name
+  summaryFile <- paste(baseDir, "SUMMARY.txt", sep="/")
+  
+  # sort the output by subject and activity - not required but easier to look at
+  dfSummary <- dfSummary[with(dfSummary, order(subject, activity)),]
+  
+  # write the filtered summary file
+  write.table(dfSummary, file=summaryFile, row.names=FALSE)
+  
+  # a bit of UI to make things friendly
+  return("Complete")    
 }
